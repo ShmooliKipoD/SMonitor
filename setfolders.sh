5febd38dc2ee66c1e61ef3a8746b90e0fa3a85a7
Samuel <sayag.samuel@live.com>
2018-11-29 16:08:40 +0200

    first commit

---
+++ setfolders.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# icons, images, etc.
+mkdir assets 
+
+# distributable files
+mkdir dist
+
+# .Net Core console application
+mkdir -p src/api
+
+# Electron + AngularJS
+mkdir -p src/app
+
+
